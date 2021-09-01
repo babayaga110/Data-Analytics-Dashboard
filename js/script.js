f686bee091966fd64eb4bb1e00d2107d7d1eaf02
babayaga110 <70019018+babayaga110@users.noreply.github.com>
2021-09-01 14:27:44 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const backBtn = document.querySelector('.back-btn');
+const  menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('nav');
+
+menuBtn.addEventListener('click', ()=>{
+    menu.style.transform = "translateX(0)"
+});
+backBtn.addEventListener('click', ()=>{
+    menu.style.transform ="translateX(-100%)"
+})
